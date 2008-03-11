b668d790bd3dfc7f73061be9ae3f080844111913
grholzi <grholzi@3cab0246-e93e-0410-9a60-bddfbd78742f>
2008-03-11 10:04:01 +0000

    removed old multiple sine example
    git-svn-id: svn://svn.code.sf.net/p/aureservoir/code@31 3cab0246-e93e-0410-9a60-bddfbd78742f

--- python/examples/slow_sine.py
+++ python/examples/slow_sine.py
@@ -1,5 +1,5 @@
 ###########################################################
-# sine genration task
+# slow sine genration task with standard ESN
 # with standard and bandpass ESNs
 #
 # 2007, Georg Holzmann
@@ -17,7 +17,8 @@ from aureservoir import *
 # FUNCTIONS
 
 def setup_STD_ESN():
-	""" configuration of a standard ESN """
+	""" configuration of a standard ESN
+	"""
 	net = DoubleESN()
 	net.setSize(20)
 	net.setInputs(1)
@@ -38,29 +39,27 @@ def setup_STD_ESN():
 	testnoise = 0.
 	return net, trainnoise, testnoise
 
-def setup_ESN_BP():
-	""" configuration of a bandpass ESN with the cutoff frequency
-	exatly at the sine frequency """
+def setup_ESN_LI():
+	""" configuration of a leaky integrating ESN """
 	net = DoubleESN()
 	net.setSize(20)
 	net.setInputs(1)
 	net.setOutputs(1)
 	net.setInitParam( CONNECTIVITY, 0.2 )
-	net.setInitParam( ALPHA, 0.3 )
-	net.setInitParam( BP_F1, 0.01 )
-	net.setInitParam( BP_F2, 0.01 )
+	net.setInitParam( ALPHA, 0.45*0.8 )
+	net.setInitParam( LEAKING_RATE, 0.8 )
 	net.setInitParam( IN_CONNECTIVITY, 0. )
 	net.setInitParam( IN_SCALE, 0. )
 	net.setInitParam( FB_CONNECTIVITY, 1. )
 	net.setInitParam( FB_SCALE, 1. )
 	net.setReservoirAct( ACT_TANH )
 	net.setOutputAct( ACT_LINEAR )
-	net.setInitAlgorithm( INIT_BP_CONST )
-	net.setSimAlgorithm( SIM_BP )
+	net.setSimAlgorithm( SIM_LI )
 	net.setTrainAlgorithm( TRAIN_PI )
 	#net.setTrainAlgorithm( TRAIN_RIDGEREG )
 	#net.setInitParam( TIKHONOV_FACTOR, 1e-4 )
-	trainnoise = 0.
+	trainnoise = 1e-6
+	#trainnoise = 0.
 	testnoise = 0.
 	return net, trainnoise, testnoise
 
@@ -113,14 +112,16 @@ trainsize = 4000
 washout = 2000
 testsize = 8000
 
+# choose ESN
+#net, trainnoise, testnoise = setup_STD_ESN()
+net, trainnoise, testnoise = setup_ESN_LI()
+net.init()
+
 # generate signals
 slsine = generate_slow_sine(trainsize+testsize)
 trainin, trainout, testin, testout = get_esn_data(slsine,trainsize,testsize)
 
 # ESN training
-#net, trainnoise, testnoise = setup_STD_ESN()
-net, trainnoise, testnoise = setup_ESN_BP()
-net.init()
 net.setNoise(trainnoise)
 net.train(trainin,trainout,washout)
 print "output weights:"
